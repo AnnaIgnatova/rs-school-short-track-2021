04511d4ac8a48ca44a90d88294b5c275cb805e7a
Anna <61065956+AnnaIgnatova@users.noreply.github.com>
2021-04-11 00:57:16 +0300

    feat: add solution

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -1,18 +1,19 @@
-/**
- * Given a sorted array, find the index of the element with the given value.
- * Time complexity should be O(logN)
- *
- * @param {Array} array
- * @param {Number} value
- * @return {Number}
- *
- * @example
- * For ([1, 2, 3], 1) should return 0
- * For ([1, 2, 3], 2) should return 1
- *
- */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
-}
+  let firstValue = 0;
+  let lastValue = array.length - 1;
+  let pos = -1;
+  let foundValue = false;
+  let middle;
 
-module.exports = findIndex;
+  while (!foundValue && firstValue <= lastValue) {
+    middle = Math.floor((firstValue + lastValue) / 2);
+    if (array[middle] === value) {
+      foundValue = true;
+      pos = middle;
+    } else 
+      if (array[middle] > value) {
+      lastValue = middle - 1;
+    } else {
+      firstValue = middle + 1;
+    }
+  }
+  return pos;
